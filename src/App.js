be7dcb0ddcbc67e1aeae7b8c51d4fda8496178b6
sidiraihan <sidiraihan@gmail.com>
2020-06-19 20:34:22 +0700

    add page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import Routes from 'react-static-routes'
 
 import './app.css'
 
+
 export default () => (
   <Router>
     <div>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
         <Link to="/blog">Blog</Link>
       </nav>
       <div className="content">
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,9 +3,34 @@ import React from 'react'
 import logoImg from '../logo.png'
 
 export default () => (
-  <div>
+  <div class="row">
     <div><h1>Hi There </h1></div>
-    <div>asdasd</div>
+<div class="flex-row">
+<div class="card">
+  <h2>Raihan Sidi Harinda</h2>
+</div>
+<div class="card">
+  <h2>Contact</h2>
+  <p>sidiraihan@gmail.com</p>
+</div>
+</div>
+<h2>Some of my project</h2>
+
+<div class="flex-row">
+<div class="card">
+  1
+</div>
+<div class="card">
+  2
+</div>
+<div class="card">
+  3
+</div>
+<div class="card">
+  4
+</div>
+</div>
+
   </div>
   
 )
