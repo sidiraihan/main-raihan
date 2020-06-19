38fbd3a052d546292eb8c2242217c05c9102fa84
sidiraihan <sidiraihan@gmail.com>
2020-06-20 00:04:02 +0700

    fix mobile

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,16 +4,16 @@ import logoImg from '../logo.png'
 
 export default () => (
   <div class="row">
-    <div><h1>Hi There </h1></div>
+    <div><h1>Hi There ✨</h1></div>
 <div class="flex-row">
 <div class="card">
   <h2>Raihan Sidi Harinda</h2>
   <p>Full time learner</p>
-  <span>ui/ux</span>  <span>database</span> <span>analysis</span> <span>seo</span> <span>problem solving</span><span>teamwork</span>
+  <div class="label"><span>ui/ux</span>  <span>database</span> <span>analysis</span> <span>seo</span> <span>teamwork</span> <span>programming</span></div>
 </div>
 <div class="card">
   <h2>Get in touch with me</h2>
-  <p>sidiraihan@gmail.com</p>
+  <a href="mailto:sidiraihan@gmail.com">sidiraihan@gmail.com</a>
   <a href="https://linkedin.com/in/raihan-sidi-harinda">linkedin.com/raihan-sidi-harinda</a>
 </div>
 </div>
