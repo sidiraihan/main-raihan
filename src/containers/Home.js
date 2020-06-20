a36d9001487465100cc7f44069ea5d24c4ec9cc7
sidiraihan <sidiraihan@gmail.com>
2020-06-20 11:03:47 +0700

    fix all

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,13 @@
 import React from 'react'
 //
 import logoImg from '../logo.png'
+import gmpImg from '../gmp.png'
+import oklayadv from '../oklayadv.png'
+import printbeta from '../printbeta.png'
+import project1 from '../project1.png'
+import project2 from '../project2.jpg'
+import project4 from '../project4.jpg'
+import ticket from '../ticket.png'
 
 export default () => (
   <div class="row">
@@ -14,24 +21,23 @@ export default () => (
 <div class="card">
   <h2>Get in touch with me</h2>
   <a href="mailto:sidiraihan@gmail.com">sidiraihan@gmail.com</a>
-  <a href="https://linkedin.com/in/raihan-sidi-harinda">linkedin.com/raihan-sidi-harinda</a>
+  <a href="https://linkedin.com/in/raihan-sidi-harinda">Linkedin</a>
+  <a href="https://github.com/sidiraihan">Github</a>
+
 </div>
 </div>
 <h2>Some of my project</h2>
 
-<div class="flex-row">
-<div class="card">
-  1
-</div>
-<div class="card">
-  2
-</div>
-<div class="card">
-  3
-</div>
-<div class="card">
-  4
-</div>
+
+<div class="flex-row project">
+
+<img src={project2} alt="" /> 
+<img src={ticket} alt="" /> 
+<img src={project1} alt="" /> 
+<img src={oklayadv} alt="" /> 
+<img src={printbeta} alt="" /> 
+<img src={project4} alt="" /> 
+<img src={gmpImg} alt="" /> 
 </div>
 
   </div>
